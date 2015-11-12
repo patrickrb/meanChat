49570e3310de97f7273ba24f31f2e50ee9771a02
Patrick Burns <pburns@kineticsupply.com>
2015-11-12 14:28:54 -0600

    chat works!

---
+++ client/components/keyBind/keyBind.directive.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('meanChatApp')
+ .constant('keyCodes', {
+        esc: 27,
+        space: 32,
+        enter: 13,
+        tab: 9,
+        backspace: 8,
+        shift: 16,
+        ctrl: 17,
+        alt: 18,
+        capslock: 20,
+        numlock: 144
+    })
+    .directive('keyBind', ['keyCodes', function (keyCodes) {
+        function map(obj) {
+            var mapped = {};
+            for (var key in obj) {
+                var action = obj[key];
+                if (keyCodes.hasOwnProperty(key)) {
+                    mapped[keyCodes[key]] = action;
+                }
+            }
+            return mapped;
+        }
+
+        return function (scope, element, attrs) {
+            var bindings = map(scope.$eval(attrs.keyBind));
+            element.bind("keydown keypress", function (event) {
+                if (bindings.hasOwnProperty(event.which)) {
+                    scope.$apply(function() {
+                         scope.$eval(bindings[event.which]);
+                    });
+                }
+            });
+        };
+    }]);
